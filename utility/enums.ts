cd830976171de040e610b43939d969b00b940fe5
jeffubayi <jeffubayi@gmail.com>
2023-08-21 14:20:07 +0300

    add enums

---
+++ utility/enums.ts
@@ -0,0 +1,39 @@
+import ImageIcon from '@mui/icons-material/Image';
+
+export const tiers = [
+  {
+    title: "Due Diligence",
+    subheader: "We are here for the long term to do property due diligence checks and title deed validation.",
+    price: "/signin",
+    description: ["Parcel errands","Tracking order"],
+    buttonText: ImageIcon,
+    buttonVariant: "outlined",
+  },
+
+  {
+    title: "Trade Easily",
+    subheader: "On alphabloQ, it is easier to enjoy real capital gains, and consistent monthly rental income.",
+    price: "/flights",
+    description: ["Storage", "Warehousing"],
+    buttonText: "View",
+    buttonVariant: "outlined",
+  },
+  {
+    title: "Capital Gains",
+    subheader: "Watch the value of your ownership grow as you go. Apart from your monthly rental income from each property.",
+    price: "/agents",
+    description: ["Pickup points", "Collection points"],
+    buttonText: "View  Agents",
+    buttonVariant: "contained",
+  },
+
+  {
+    title: "Inflation Hedge",
+    subheader: "Real estate is the oldest asset class in history and has always been a great hedge during inflation.",
+    price: "/flights",
+    description: ["Storage", "Warehousing"],
+    buttonText: "View",
+    buttonVariant: "outlined",
+  },
+  
+];
