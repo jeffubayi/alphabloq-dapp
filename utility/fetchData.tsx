1252ffba4548f2a5697ed4dff1c9fd44e56e3ea7
jeffubayi <jeffubayi@gmail.com>
2023-08-21 14:19:57 +0300

    add reusable fetch utils

---
+++ utility/fetchData.tsx
@@ -0,0 +1,8 @@
+
+import { supabase } from "./supabaseClient";
+
+const fetchData = async (collection: string) => {
+    const { data } = await supabase.from(`${collection}`).select(`*`)
+    return data
+}
+export default fetchData
