f31008bbd3d84e47a5d16fb75e09fe931d9167db
jeffubayi <jeffubayi@gmail.com>
2023-08-23 21:07:52 +0300

    feat:add smart contract

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./pages/artifacts"
+  },
+};
