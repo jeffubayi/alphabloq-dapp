b413e03ca1a16e9e9c3f83e5949ec2228e299465
jeffubayi <jeffubayi@gmail.com>
2023-08-30 07:56:39 +0300

    add user data

--- redux/hooks.ts
+++ redux/hooks.ts
@@ -1,16 +1,15 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { JokesState,JokesValues} from "../types";
 
 export const retoolApi = createApi({
   reducerPath: "retoolApi",
   baseQuery: fetchBaseQuery({ baseUrl: "https://mernappbackend.onrender.com" }),
   tagTypes: ["Jokes"],
   endpoints: (builder) => ({
-    getJokes: builder.query<JokesState[], void>({
+    getJokes: builder.query<any[], void>({
       query: () => "/user/details",
       providesTags: ["Jokes"],
     }),
-    createJoke: builder.mutation<void, Partial<JokesValues>>({
+    createJoke: builder.mutation<void, Partial<any>>({
       query: (newJoke) => ({
         url: "jokes",
         method: 'POST',
@@ -18,14 +17,14 @@ export const retoolApi = createApi({
       }),
       invalidatesTags: ["Jokes"],
     }),
-    deleteJoke: builder.mutation<JokesState[], number | string | string[] | undefined>({
+    deleteJoke: builder.mutation<any[], number | string | string[] | undefined>({
       query: (id) => ({
         url: `jokes/${id}`,
         method: 'DELETE',
       }),
       invalidatesTags: ["Jokes"],
     }),
-    updateJoke: builder.mutation<JokesState[], { id: number | string | string[] | undefined; values: Partial<JokesValues> }>({
+    updateJoke: builder.mutation<any[], { id: number | string | string[] | undefined; values: Partial<any> }>({
       query: ({ id, values }) => ({
         url: `jokes/${id}`,
         method: 'PUT',
